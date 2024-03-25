4ff13f47e352fa282ace1f897a29d763071b5003
Martin Casares <casaresrm@gmail.com>
2024-03-25 17:54:29 -0300

    product-detail

--- src/ProtectedRoute2.jsx
+++ src/ProtectedRoute2.jsx
@@ -4,7 +4,7 @@ import { Navigate, Outlet } from 'react-router-dom';
 export const ProtectedRoute2 = ({ children }) => {
 	const user = JSON.parse(localStorage.getItem('user')) || null;
 
-	if (!user || !user.rol || user.rol !== 'user') {
+	if (!user || !user.rol || (user.rol !== 'user' && user.rol !== 'admin')) {
 		return <Navigate to="/" />;
 	} else {
 		return <Outlet />;
